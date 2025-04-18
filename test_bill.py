227ba546768be9dd50824d9ec9d7628cfa0c2ad0
Arushi9740 <arushimuskan004@gmail.com>
2025-04-18 12:52:44 +0530

    second question files

--- test_bill.py
+++ test_bill.py
@@ -1,6 +1,9 @@
 from bill import get_bill
 
-# Test valid
-assert get_bill("P001") == {"name": "Alice", "amount": 700}
+def test_get_bill_valid():
+    assert get_bill("P001") == {"name": "Alice", "amount": 700}
 
-print("Test passed.")
+def test_get_bill_invalid():
+    assert get_bill("XYZ") == {}
+
+print("test passed")
